614c627ae52f66dd9f9780a409c7bf26599ee9ee
Aaditi <aadi2007.aaditi@gmail.com>
2020-07-28 21:29:12 -0700

    fix null or empty name issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,9 +62,20 @@ var fight = function(pickedEnemyObj){
     }
 };
 
+var getPlayerName = function(){
+    var name = "";
+
+    while(name === "" || name === null){
+        name = window.prompt("What is your robot's name?");
+    }
+    
+    
+    console.log("Your robot's name is " + name);
+    return name;
+}
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
